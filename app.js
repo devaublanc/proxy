7d2ec3c035a4bd393a03fb9b72d8c3d46842b148
Charles Jacquin <charles.jacquin@autistici.org>
2016-11-15 09:57:08 +0100

    add ternary to determine the correct url

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ app.get('*', function (req, res) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    request('http://medusa.dev.ifeelsmart.net' + req.path + '?device=62', function (error, response, body) {
+    const isDev = req.path.indexOf('contents') === -1
+    request(`http://medusa.${ isDev ? 'dev': 'stg' }.ifeelsmart.net${req.path}?device=62`, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             res.setHeader('Content-Type', 'application/json');
             res.send(body);
+        } else {
+            console.error(error, response.statusCode)
         }
     })
-
 });
 
 app.listen(3001, function () {
